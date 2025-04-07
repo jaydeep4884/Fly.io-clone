affffe2d0f271b654b251fcc0a7ec5155946b688
SagathiyaJaydeep <sagathiyajaydeep66@gmail.com>
2025-04-07 11:13:10 +0530

    Trusted Section Started...

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "@fontsource/bricolage-grotesque";
 import Header from "./sections/Header";
 import Developers from "./sections/Developers";
 import Footer from "./sections/Footer";
+import Trusted from "./sections/Trusted";
 
 function App() {
   return (
     <div>
       <Header />
+      <Trusted />
       <Developers />
       <Footer />
     </div>
---
+++ src/sections/Trusted.jsx
@@ -0,0 +1,20 @@
+import { Box, Container } from "@mui/material";
+import React from "react";
+
+function Trusted() {
+  return (
+    <Box className="my-32">
+      <Container maxWidth>
+        <Box aria-label="Global" className="mx-auto  max-w-7xl  lg:px-8">
+          <Box>
+            <h2 className="uppercase text-center text-violet-700 tracking-wider font-bold mb-6 text-sm">
+              Trusted by teams at
+            </h2>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+}
+
+export default Trusted;
